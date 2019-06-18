fe300a42b4a09ccefc8719438a2c9afb454f0f7c
ktaebum <phya.ktaebum@gmail.com>
2019-06-18 16:01:05 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+### zsh link
+ln -s ~/.dotfiles/zsh/zshrf ~/.zshrc
+
+### tmux link
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+### vimrc link
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+
+### style files
+ln -s ~/.dotfiles/styles/clang-format ~/.clang-format
+ln -s ~/.dotfiles/styles/style_yapf ~/.style.yapf
