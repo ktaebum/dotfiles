e74b519373fe02a63a7bd20a585792de055ed3e3
ktaebum <phya.ktaebum@gmail.com>
2019-06-18 07:31:13 +0000

    some plugin updates

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 ### zsh link
-ln -s ~/.dotfiles/zsh/zshrf ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -L -o ~/.oh-my-zsh/custom/themes/materialshell.zsh-theme https://raw.githubusercontent.com/carloscuesta/materialshell/master/materialshell.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm ~/.zshrc
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 ### tmux link
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
@@ -10,6 +15,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.dotfiles/vim/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 vim +PluginInstall +qall
 
+### YCM install
 python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 ### style files
