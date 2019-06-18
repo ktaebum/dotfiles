b7d40cb84f79eb72e1a67a85061a0dd39499ae62
ktaebum <phya.ktaebum@gmail.com>
2019-06-18 07:21:36 +0000

    add modules

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 ### vimrc link
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.dotfiles/vim/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+vim +PluginInstall +qall
+
+python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 ### style files
 ln -s ~/.dotfiles/styles/clang-format ~/.clang-format
