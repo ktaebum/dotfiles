31a79757ccb6c3816897df1a4bf77b737d4a5ab6
ktaebum <ktaebum@friendli.ai>
2023-08-21 23:03:36 +0900

    minor

--- vim/init.lua
+++ vim/init.lua
@@ -4,8 +4,9 @@ require("settings")
 require("plugins")
 require("maps")
 
--- setup must be called before loading the colorscheme
--- Default options:
+-- Colorschemes
+
+-- gruvbox
 require("gruvbox").setup({
   undercurl = true,
   underline = true,
@@ -28,4 +29,85 @@ require("gruvbox").setup({
   dim_inactive = false,
   transparent_mode = false,
 })
-vim.cmd("colorscheme gruvbox")
+
+-- kanagawa
+require("kanagawa").setup({
+  compile = false,             -- enable compiling the colorscheme
+  undercurl = true,            -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true},
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = false,         -- do not set background color
+  dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+  terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+  colors = {                   -- add/modify theme and palette colors
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {}
+  end,
+  theme = "wave",              -- Load "wave" theme when 'background' option is not set
+  background = {               -- map the value of 'background' option to a theme
+    dark = "wave",           -- try "dragon" !
+    light = "lotus"
+  },
+})
+
+-- ayu
+require('ayu').setup({
+  mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+})
+
+-- monokai-pro
+require("monokai-pro").setup({
+  transparent_background = false,
+  terminal_colors = true,
+  devicons = true, -- highlight the icons of `nvim-web-devicons`
+  styles = {
+    comment = { italic = true },
+    keyword = { italic = true }, -- any other keyword
+    type = { italic = true }, -- (preferred) int, long, char, etc
+    storageclass = { italic = true }, -- static, register, volatile, etc
+    structure = { italic = true }, -- struct, union, enum, etc
+    parameter = { italic = true }, -- parameter pass in function
+    annotation = { italic = true },
+    tag_attribute = { italic = true }, -- attribute of tag in reactjs
+  },
+  filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+  -- Enable this will disable filter option
+  day_night = {
+    enable = false, -- turn off by default
+    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+  },
+  inc_search = "background", -- underline | background
+  background_clear = {
+    -- "float_win",
+    "toggleterm",
+    "telescope",
+    -- "which-key",
+    -- "renamer",
+    -- "notify",
+    "nvim-tree",
+    -- "neo-tree",
+    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+  },-- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+  plugins = {
+    bufferline = {
+      underline_selected = false,
+      underline_visible = false,
+    },
+    indent_blankline = {
+      context_highlight = "default", -- default | pro
+      context_start_underline = false,
+    },
+  },
+  ---@param c Colorscheme
+  override = function(c) end,
+})
+
+vim.cmd("colorscheme ayu")
--- vim/lua/configs/nvim-lsp.lua
+++ vim/lua/configs/nvim-lsp.lua
@@ -23,8 +23,8 @@ for _, lsp in ipairs(servers) do
   })
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false
-  }
-)
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  -- vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- virtual_text = false
+  -- }
+-- )
