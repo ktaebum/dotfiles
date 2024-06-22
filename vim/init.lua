210807fa826a454c67968402cd8b088425f838bd
Taebum Kim <ktaebum@friendli.ai>
2024-06-23 05:58:00 +0900

    some updates

--- vim/init.lua
+++ vim/init.lua
@@ -128,7 +128,7 @@ require("github-theme").setup({
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "day", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
@@ -144,9 +144,9 @@ require("tokyonight").setup({
     floats = "normal", -- style for floating windows
   },
   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = true, -- dims inactive windows
+  day_brightness = 0.2, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
   lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
   --- You can override specific color groups to use other groups or a hex color
@@ -211,6 +211,45 @@ require("catppuccin").setup({
         indentscope_color = "",
     },
     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+require("cyberdream").setup({
+  -- Enable transparent background
+  transparent = false,
+
+  -- Enable italics comments
+  italic_comments = true,
+
+  -- Replace all fillchars with ' ' for the ultimate clean look
+  hide_fillchars = false,
+
+  -- Modern borderless telescope theme
+  borderless_telescope = true,
+
+  -- Set terminal colors used in `:terminal`
+  terminal_colors = true,
+
+  theme = {
+    variant = "default", -- use "light" for the light variant
+    highlights = {
+      -- Highlight groups to override, adding new groups is also possible
+      -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+      -- Example:
+      Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+      -- Complete list can be found in `lua/cyberdream/theme.lua`
+    },
+
+    -- Override a color entirely
+    colors = {
+      -- For a list of colors see `lua/cyberdream/colours.lua`
+      -- Example:
+      bg = "#000000",
+      green = "#00ff00",
+      magenta = "#ff00ff",
+    },
   },
 })
 
