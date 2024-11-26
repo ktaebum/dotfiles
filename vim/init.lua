3135a437c1d8ca8ab513c2583da91deba3d61134
Taebum Kim <ktaebum@friendli.ai>
2024-11-26 10:55:55 +0900

    minor updates

--- vim/init.lua
+++ vim/init.lua
@@ -107,7 +107,7 @@ require("github-theme").setup({
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
@@ -269,4 +269,4 @@ require("rose-pine").setup({
 })
 
 vim.cmd("let g:everforest_background = 'hard'")
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme kanagawa")
