7f8adedd55e1564207201ae901a116c033319bd2
Taebum Kim <ktaebum@friendli.ai>
2024-12-01 17:06:04 +0900

    material

--- vim/init.lua
+++ vim/init.lua
@@ -293,5 +293,68 @@ require("gruvbox").setup({
   transparent_mode = false,
 })
 
+require('material').setup({
+
+    contrast = {
+        terminal = false, -- Enable contrast for the built-in terminal
+        sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = true, -- Enable contrast for floating windows
+        cursor_line = true, -- Enable darker background for the cursor line
+        lsp_virtual_text = true, -- Enable contrasted background for lsp virtual text
+        non_current_windows = true, -- Enable contrasted background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+    },
+
+    styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { italic = true },
+        strings = { bold = true },
+        keywords = { --[[ underline = true ]] },
+        functions = { bold = true, undercurl = true },
+        variables = {},
+        operators = {},
+        types = {},
+    },
+
+    plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        -- "coc",
+        -- "colorful-winsep",
+        -- "dap",
+        -- "dashboard",
+        -- "eyeliner",
+        -- "fidget",
+        -- "flash",
+        "gitsigns",
+        -- "harpoon",
+        -- "hop",
+        -- "illuminate",
+        "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        -- "neogit",
+        -- "neotest",
+        -- "neo-tree",
+        -- "neorg",
+        -- "noice",
+        "nvim-cmp",
+        -- "nvim-navic",
+        -- "nvim-tree",
+        "nvim-web-devicons",
+        -- "rainbow-delimiters",
+        -- "sneak",
+        "telescope",
+        -- "trouble",
+        "which-key",
+        -- "nvim-notify",
+    },
+
+    high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = false -- Enable higher contrast text for darker style
+    },
+
+    lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+})
+
 vim.cmd("let g:everforest_background = 'hard'")
-vim.cmd("colorscheme vscode")
+vim.cmd("colorscheme material-darker")
