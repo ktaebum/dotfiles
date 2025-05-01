bde51d953ca6395d4b8cf892fa25d293d4d0a8ea
Taebum Kim <ktaebum@friendli.ai>
2025-05-01 14:37:32 +0900

    lazy telescope

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -35,6 +35,7 @@ return {
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
