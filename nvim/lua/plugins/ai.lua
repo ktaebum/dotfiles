e0a4a8ed284b0cb9d25c37ac08094a356be97ec1
Taebum Kim <ktaebum@friendli.ai>
2025-05-16 12:10:33 +0900

    use nvim-cmp

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -31,7 +31,7 @@ return {
       --- The below dependencies are optional,
       "echasnovski/mini.pick", -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       -- "zbirenbaum/copilot.lua", -- for providers='copilot'
