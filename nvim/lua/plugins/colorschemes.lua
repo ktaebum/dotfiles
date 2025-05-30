4f8b10a3d69ee7dadbb9bc6c88655c6b432dcdf0
Taebum Kim <ktaebum@friendli.ai>
2025-05-30 23:55:17 +0900

    kanagawa-paper

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -170,8 +170,69 @@ return {
       -- vim.cmd.colorscheme("kanagawa-paper-ink")
     end,
     opts = {
-      dim_inactive = true, -- dim inactive windows
-      undercurl = false, -- use undercurl instead of underline for diagnostics
-    },
+      -- enable undercurls for underlined text
+      undercurl = false,
+      -- transparent background
+      transparent = false,
+      -- highlight background for the left gutter
+      gutter = false,
+      -- background for diagnostic virtual text
+      diag_background = true,
+      -- dim inactive windows. Disabled when transparent
+      dim_inactive = true,
+      -- set colors for terminal buffers
+      terminal_colors = true,
+      -- cache highlights and colors for faster startup.
+      -- see Cache section for more details.
+      cache = false,
+
+      styles = {
+        -- style for comments
+        comment = { italic = true },
+        -- style for functions
+        functions = { italic = false },
+        -- style for keywords
+        keyword = { italic = false, bold = true },
+        -- style for statements
+        statement = { italic = false, bold = false },
+        -- style for types
+        type = { italic = false },
+      },
+      -- override default palette and theme colors
+      colors = {
+        palette = {},
+        theme = {
+          ink = {},
+          canvas = {},
+        },
+      },
+      -- adjust overall color balance for each theme [-1, 1]
+      color_offset = {
+        ink = { brightness = 0, saturation = 0 },
+        canvas = { brightness = 0, saturation = 0 },
+      },
+      -- override highlight groups
+      overrides = function(colors)
+        return {}
+      end,
+
+      -- uses lazy.nvim, if installed, to automatically enable needed plugins
+      auto_plugins = true,
+      -- enable highlights for all plugins (disabled if using lazy.nvim)
+      all_plugins = package.loaded.lazy == nil,
+      -- manually enable/disable individual plugins.
+      -- check the `groups/plugins` directory for the exact names
+      plugins = {
+        -- examples:
+        -- rainbow_delimiters = true
+        -- which_key = false
+      },
+    }
+  },
+  {
+    "wtfox/jellybeans.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {}, -- Optional
   }
 }
