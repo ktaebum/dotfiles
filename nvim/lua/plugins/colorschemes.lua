dc86454fe9209359935b40decc0073fc4a03bc80
Taebum Kim <ktaebum@friendli.ai>
2025-07-03 09:42:13 +0900

    tokyonight

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -10,7 +10,7 @@ return {
       tokyonight.setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         light_style = "day", -- The theme is used when the background is set to light
         transparent = false, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
@@ -58,7 +58,7 @@ return {
           -- telescope = true,
         },
       })
-      -- vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme tokyonight]])
     end,
   },
   {
@@ -112,7 +112,7 @@ return {
           background = true, -- use background color for virtual text
         },
       }
-      require('bamboo').load()
+      -- require('bamboo').load()
     end,
   },
   {
