d35eff203ae5527505e1df9f1f4947805c59b31c
Taebum Kim <ktaebum@friendli.ai>
2025-07-17 21:59:13 +0900

    treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,11 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
+    branch = "master",
     build = ":TSUpdate",
     -- event = { "LazyFile", "VeryLazy" },
-    event = "VeryLazy",
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    -- event = "VeryLazy",
+    lazy = false, -- vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     init = function(plugin)
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
@@ -14,51 +15,61 @@ return {
       -- during startup.
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        opts_extend = { "ensure_installed" },
-        ensure_installed = {
-          "c",
-          "cmake",
-          "cpp",
-          "cuda",
-          "gitcommit",
-          "gitignore",
-          "git_config",
-          "git_rebase",
-          "lua",
-          "python",
-          "vim",
-          "lua",
-          "yaml",
-          "regex",
-          "json",
-          "proto"
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        auto_install = true,
-        sync_install = true,
-        indent = {
-          enable = true,
-          disable = {"python"},
-        }
-      })
     end,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      opts_extend = { "ensure_installed" },
+      ensure_installed = {
+        "c",
+        "cmake",
+        "cpp",
+        "cuda",
+        "gitcommit",
+        "gitignore",
+        "git_config",
+        "git_rebase",
+        "lua",
+        "python",
+        "vim",
+        "lua",
+        "yaml",
+        "regex",
+        "json",
+        "proto"
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+        },
+      },
+      indent = {
+        enable = true,
+      }
+    },
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     keys = {
       { "<c-space>", desc = "Increment Selection" },
       { "<bs>", desc = "Decrement Selection", mode = "x" },
     },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   }
 }
