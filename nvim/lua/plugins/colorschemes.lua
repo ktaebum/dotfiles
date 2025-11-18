9d73185fffaef31e3af5fb7349c72cc273fb3d05
Taebum Kim <ktaebum@friendli.ai>
2025-11-18 02:34:34 +0000

    kanagawa

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -238,7 +238,30 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
       require("kanagawa").setup({
+        compile = false,             -- enable compiling the colorscheme
+        undercurl = false,            -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { bold = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,         -- do not set background color
+        dimInactive = true,         -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+        colors = {                   -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",              -- Load "wave" theme
+        background = {               -- map the value of 'background' option to a theme
+          dark = "wave",           -- try "dragon" !
+          light = "lotus"
+        },
       })
+      -- vim.cmd([[colorscheme kanagawa]])
     end
   },
   {
