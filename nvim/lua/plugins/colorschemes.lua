48cc85fb4f5318df65e40a66f06eec546b57c0ec
Taebum Kim <ktaebum@friendli.ai>
2026-02-09 05:40:17 +0000

    main

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -143,7 +143,7 @@ return {
     config = function()
       require("rose-pine").setup({
         variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "moon", -- main, moon, or dawn
+        dark_variant = "main", -- main, moon, or dawn
         dim_inactive_windows = true,
         extend_background_behind_borders = true,
 
