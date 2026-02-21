4f968dda84381c0ec49766ae71b057100857ce86
Taebum Kim <ktaebum@friendli.ai>
2026-02-21 14:14:47 +0000

    adopt catppuccin variant

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -284,7 +284,7 @@ return {
         flavour = "auto", -- latte, frappe, macchiato, mocha
         background = { -- :h background
           light = "latte",
-          dark = "mocha",
+          dark = "macchiato",
         },
         transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
@@ -312,7 +312,68 @@ return {
           operators = {},
           -- miscs = {}, -- Uncomment to turn off hard-coded styles
         },
-        custom_highlights = {},
+        color_overrides = {
+          macchiato = {
+            rosewater = "#F5B8AB",
+            flamingo = "#F29D9D",
+            pink = "#AD6FF7",
+            mauve = "#FF8F40",
+            red = "#E66767",
+            maroon = "#EB788B",
+            peach = "#FAB770",
+            yellow = "#FACA64",
+            green = "#70CF67",
+            teal = "#4CD4BD",
+            sky = "#61BDFF",
+            sapphire = "#4BA8FA",
+            blue = "#00BFFF",
+            lavender = "#00BBCC",
+            text = "#C1C9E6",
+            subtext1 = "#A3AAC2",
+            subtext0 = "#8E94AB",
+            overlay2 = "#7D8296",
+            overlay1 = "#676B80",
+            overlay0 = "#464957",
+            surface2 = "#3A3D4A",
+            surface1 = "#2F313D",
+            surface0 = "#1D1E29",
+            base = "#0b0b12",
+            mantle = "#11111a",
+            crust = "#191926",
+          },
+          integrations = {
+            telescope = {
+              enabled = true,
+              style = "nvchad",
+            },
+          },
+        },
+        custom_highlights = function(colors)
+          return {
+            ["@module"] = { fg = colors.rosewater, style = { "bold" } },
+            CurSearch = { bg = colors.sky },
+            IncSearch = { bg = colors.sky },
+            CursorLineNr = { fg = colors.blue, style = { "bold" } },
+            DashboardFooter = { fg = colors.overlay0 },
+            TreesitterContextBottom = { style = {} },
+            WinSeparator = { fg = colors.overlay0, style = { "bold" } },
+            ["@markup.italic"] = { fg = colors.blue, style = { "italic" } },
+            ["@markup.strong"] = { fg = colors.blue, style = { "bold" } },
+            Headline = { style = { "bold" } },
+            Headline1 = { fg = colors.blue, style = { "bold" } },
+            Headline2 = { fg = colors.pink, style = { "bold" } },
+            Headline3 = { fg = colors.lavender, style = { "bold" } },
+            Headline4 = { fg = colors.green, style = { "bold" } },
+            Headline5 = { fg = colors.peach, style = { "bold" } },
+            Headline6 = { fg = colors.flamingo, style = { "bold" } },
+            rainbow1 = { fg = colors.blue, style = { "bold" } },
+            rainbow2 = { fg = colors.pink, style = { "bold" } },
+            rainbow3 = { fg = colors.lavender, style = { "bold" } },
+            rainbow4 = { fg = colors.green, style = { "bold" } },
+            rainbow5 = { fg = colors.peach, style = { "bold" } },
+            rainbow6 = { fg = colors.flamingo, style = { "bold" } },
+          }
+        end,
         default_integrations = true,
         integrations = {
           cmp = true,
@@ -328,7 +389,7 @@ return {
         },
       })
       -- setup must be called before loading
-      -- vim.cmd([[colorscheme catppuccin]])
+      vim.cmd([[colorscheme catppuccin]])
     end,
   },
   {
@@ -385,7 +446,7 @@ return {
         },
         groups = {},
       })
-      vim.cmd([[colorscheme carbonfox]])
+      -- vim.cmd([[colorscheme carbonfox]])
     end,
   }, -- lazy
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
