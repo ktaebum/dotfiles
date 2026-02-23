720025b10cf667d3be87b014befb0a6aba04545a
Taebum Kim <ktaebum@friendli.ai>
2026-02-23 03:42:04 +0000

    enable underline

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -296,7 +296,7 @@ return {
         },
         no_italic = false, -- Force no italic
         no_bold = false, -- Force no bold
-        no_underline = true, -- Force no underline
+        no_underline = false, -- Force no underline
         styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
           comments = { "italic" }, -- Change the style of comments
           conditionals = { "bold" },
